12a54d4e0c370d491ba1d744149b554ada1ee438
Metin <meto260@gmail.com>
2022-06-26 01:17:17 +0300

    pgsql works

---
+++ QueryFilter.js
@@ -0,0 +1,59 @@
+const { Pool } = require('pg');
+const connectionString = '${{ secrets.CON_STR }}';
+const db = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
+
+class QueryFilter {
+    constructor() {
+        async () => await db.connect();
+    }
+    findInList(rq) {
+        const { keyname, arraydata } = rq;
+        let found = arraydata.find(x => x.key === keyname);
+        if (arraydata.includes(found))
+            return { success: true, found };
+        else if (keyname.length > 0) {
+            let flter = arraydata.map(x => x.id);
+            let nextid = Math.max(...flter) + 1;
+            arraydata.push({
+                id: nextid,
+                time: new Date(),
+                key: keyname
+            });
+            return { success: true }
+        }
+        return { success: false }
+    }
+
+    async searchInCodes(rq) {
+        try {
+            const { country, zip, nbhood, city, dist } = rq;
+            let query = "SELECT * FROM postalcode";
+            let where = [];
+            if (!(!(country))) where.push("countrycode LIKE '%" + country + "%'");
+            if (!(!(zip))) where.push("\"postalcode\" = '" + zip + "'");
+            if (!(!(nbhood))) where.push("mahalle LIKE '%" + nbhood + "%'");
+            if (!(!(city))) where.push("city LIKE '%" + city + "%'");
+            if (!(!(dist))) where.push("ilce LIKE '%" + dist + "%'");
+
+            if (where.length > 0) {
+                query += " WHERE";
+                let ix = 0;
+                where.forEach(w => {
+                    if (ix > 0) query += " AND";
+                    query += ' ' + w;
+                    ix++;
+                });
+                query += " LIMIT 10";
+            }
+            let found = await db.query(query);
+            if (found.rows.length > 0)
+                return { success: true, data: found.rows };
+            else
+                return { success: false, data: "nothing found" };
+
+        } catch (err) {
+            return { success: false, data: err };
+        }
+    }
+}
+module.exports = QueryFilter;
--- index.js
+++ index.js
@@ -5,41 +5,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+const QueryFilter = require('./QueryFilter.js');
 
-app.listen(port, () =>
-    console.log(`API is running on: http://localhost:${port}.`)
-);
-
-var memDatas = [{
-    id: 1,
-    time: new Date(),
-    key: "istanbul"
-}, {
-    id: 2,
-    time: new Date(),
-    key: "ankara"
-}];
+app.listen(port, () => {
+    console.log(`API is running on: http://localhost:${port}.`);
+});
 
 app.get('/', (request, response) => {
-    // The string we want to display on http://localhost:3000
-    response.send('Welcome on the books API! Take a breath and start using it!')
+    response.send('Welcome on the zipcode API! Take a breath and start using it!')
+});
+
+app.get('/q', async (request, response)=>{
+    let result = await new QueryFilter().searchInCodes(request.query);
+    return response.json(result);
 });
 
 app.get('/find', (request, response) => {
-    const keyname = request.query.key;
-    let found = memDatas.find(x=>x.key===keyname);
-    if (memDatas.includes(found))
-        return response.json(found);
-    else if (keyname.length > 0) {
-        let flter = memDatas.map(x=>x.id);
-        let nextid = Math.max(...flt) + 1;
-        memDatas.push({
-            id: nextid,
-            time: new Date(),
-            key: keyname
-        });
-        return response.json({ success: true })
-    } 
-    return response.json({ success: false }) 
+    let result = new QueryFilter().findInList(request.query);
+    return response.json(result);
 });
-
