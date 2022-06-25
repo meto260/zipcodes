1d9145222512edfa3030e787cabe68d4f478671f
Metin <meto260@gmail.com>
2022-06-26 01:56:32 +0300

    express problem testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 const QueryFilter = require('./QueryFilter.js');
 
